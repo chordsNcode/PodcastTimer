325d83f288f975a1773c805c8ab629d3e7978ad5
Matt Dias <matt.dias6@gmail.com>
2017-11-25 19:28:52 -0500

    basic UI

---
+++ PodcastTimer/Views/CountDownViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CountDownViewController.swift
+//  PodcastTimer
+//
+//  Created by Matt Dias on 11/25/17.
+//  Copyright © 2017 Matt Dias. All rights reserved.
+//
+
+import UIKit
+
+class CountDownViewController: UIViewController {
+    
+    @IBOutlet var timeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapStartStop(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapReset(_ sender: UIButton) {
+    }
+
+}
